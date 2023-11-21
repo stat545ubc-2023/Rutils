723eee89e3a40cb1694c9a0f91572faaf9e72b57
giankdiluvi <giankdiluvi@gmail.com>
2023-11-20 21:32:15 -0800

    add expm1 tests

--- tests/testthat/test-Expm1.R
+++ tests/testthat/test-Expm1.R
@@ -1,10 +1,10 @@
 testthat::test_that("Input has to be numeric",{
-  testthat::expect_error(logsumexp(c(TRUE,FALSE))) # boolean input
-  testthat::expect_error(logsumexp(c(2,"3"))) # string input
+  testthat::expect_error(Expm1(c(TRUE,FALSE))) # boolean input
+  testthat::expect_error(Expm1(c(2,"3"))) # string input
 })
 
 testthat::test_that("Output behaves as expected",{
-  testthat::expect_equal(3,logsumexp(3)) # logsumexp(float) = float
-  testthat::expect_length(logsumexp(1:100),1) # logsumexp(vector) is float
-  testthat::expect_gt(logsumexp(-10^(10:15)),-Inf) # handles large negative numbers
+  testthat::expect_equal(0,Expm1(log(1))) # Expm1(log(1))=exp(0)-1=0
+  testthat::expect_length(Expm1(1:100),100) # Expm1(vector) is vector of same size
+  testthat::expect_gt(Expm1(10^(-(10:15))),0) # handles small positive numbers
 })
